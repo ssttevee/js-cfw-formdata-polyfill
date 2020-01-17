e3612bb30372cad02ea000c3d0c3922780a5833f
Steve Lam <slam@akshonesports.com>
2020-01-17 15:42:13 -0800

    add method to use alternate parser as fallback

--- src/ponyfill.ts
+++ src/ponyfill.ts
@@ -6,23 +6,28 @@ import { arrayToString } from '@ssttevee/u8-utils';
 const RE_MULTIPART = /^multipart\/form-data(?:;\s*boundary=(?:"((?:[^"]|\\")+)"|([^\s;]+)))$/;
 const RE_URLENCODED = /^application\/x-www-form-urlencoded(?:;|$|\s)/;
 
-const nativeFormDataImpl = Request.prototype.formData;
-export default async function(this: Request): Promise<FormData> {
-    const contentType = this.headers.get('content-type');
-    const matches = RE_MULTIPART.exec(contentType);
-    if (matches && (matches[1] || matches[2])) {
-        const parts = await parseMultipart(this.body, matches[1] || matches[2]);
-        const fd = new FormData();
-        for (const { name, data, filename, contentType } of parts) {
-            fd.append(name, filename ? new Blob([data], { type: contentType }) : arrayToString(data), filename);
+type Parser = (this: Request) => Promise<FormData>;
+
+export function makeWrapper(fallbackParser: Parser): Parser {
+    return async function(this: Request): Promise<FormData> {
+        const contentType = this.headers.get('content-type');
+        const matches = RE_MULTIPART.exec(contentType);
+        if (matches && (matches[1] || matches[2])) {
+            const parts = await parseMultipart(this.body, matches[1] || matches[2]);
+            const fd = new FormData();
+            for (const { name, data, filename, contentType } of parts) {
+                fd.append(name, filename ? new Blob([data], { type: contentType }) : arrayToString(data), filename);
+            }
+
+            return fd;
         }
-        
-        return fd;
-    }
 
-    if (contentType.match(RE_URLENCODED)) {
-        return nativeFormDataImpl.call(this);
-    }
+        if (contentType.match(RE_URLENCODED)) {
+            return fallbackParser.call(this);
+        }
 
-    throw new TypeError('Unexpected Content-Type: ' + contentType);
+        throw new TypeError('Unexpected Content-Type: ' + contentType);
+    }
 }
+
+export default makeWrapper(Request.prototype.formData as any);
